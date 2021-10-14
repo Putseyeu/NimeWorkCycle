93521526f96a493c4738b9734b8b41e53d54a186
Putseyeu <putseyeu.a.d@gmail.com>
2021-10-14 18:30:13 +0200

    step1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomeWorkCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageUser;
+            int totalRepeat;
+
+            Console.Write("What's your message ?: ");
+            messageUser = Console.ReadLine();
+            Console.Write("How many times to repeat it ?:");
+            totalRepeat = Convert.ToInt32(Console.ReadLine());
+
+            while ( totalRepeat --> 0)
+            {
+                Console.WriteLine(messageUser);
+            }
+        }
+
+    }
+}
